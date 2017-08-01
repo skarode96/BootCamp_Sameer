3354dc8d4e76af40e1d7f91b99c3c10142e527c7
DS\karodes <skarode96@gmail.com>
2017-08-01 06:12:29 +0530

    concurrent file reading exercise done

---
+++ Day11/ConcurrentFileReading/src/com/cdk/threads/FileReader2.java
@@ -0,0 +1,36 @@
+package com.cdk.threads;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by karodes on 7/31/2017.
+ */
+public class FileReader2 implements Runnable {
+    @Override
+    public void run() {
+        InputStream fis = null;
+        try {
+            fis = new FileInputStream("C:\\Users\\karodes\\Documents\\BootCamp_Sameer\\Day11\\ConcurrentFileReading\\src\\com\\cdk\\files\\story2.txt");
+            int b = fis.read();
+            while(b != -1) {
+                System.out.print((char)b);
+                b = fis.read();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null)
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+
+    }
+}
