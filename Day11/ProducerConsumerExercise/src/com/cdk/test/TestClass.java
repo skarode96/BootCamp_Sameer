9439165e94b53e02fabded39392675b90a4bc04b
DS\karodes <skarode96@gmail.com>
2017-08-01 07:24:24 +0530

    ProducerConsumer problem studied

---
+++ Day11/ProducerConsumerExercise/src/com/cdk/test/TestClass.java
@@ -0,0 +1,31 @@
+package com.cdk.test;
+
+import org.junit.Test;
+
+/**
+ * Created by karodes on 7/31/2017.
+ */
+public class TestClass {
+    public int i = 0;
+
+    @Test
+    public void test1() {
+        Thread incrementorWorker = new Thread(() -> {
+            int j = 0;
+            while (j < 200000000) {
+                TestClass.this.i++;
+                j++;
+            }
+        });
+
+        Thread displayWorker = new Thread(() -> {
+            int j = 0;
+            while (j < 20000) {
+                System.out.println("Value of i : " + this.i);
+                j++;
+            }
+        });
+        incrementorWorker.start();
+        displayWorker.start();
+    }
+}
