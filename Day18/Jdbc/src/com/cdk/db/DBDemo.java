972292b167149b85aaaa1c176a8ee7824d382895
DS\karodes <skarode96@gmail.com>
2017-08-08 23:34:30 +0530

    Jdbc Demo done

---
+++ Day18/Jdbc/src/com/cdk/db/DBDemo.java
@@ -0,0 +1,57 @@
+package com.cdk.db;
+
+import java.sql.*;
+
+class DBDemo {
+
+    public static void main(String[] s) {
+
+        Connection con = null;
+        Statement stm = null;
+        ResultSet rs = null;
+        try {
+            //Load jdbc driver in jvm
+            Class.forName("com.mysql.jdbc.Driver");
+            //Create a DB connection
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cdk?useSSL=false", "root", "root");
+            System.out.println("Connected to DB");
+            //Create a statement
+            stm = con.createStatement();
+            //Run the SQL query
+//            stm.executeUpdate("INSERT INTO employee_master VALUES (1,'Jaggu',453345.00)");
+//            stm.executeUpdate("UPDATE employee_master SET e_salary = 10000.00 WHERE e_id = 1");
+            rs = stm.executeQuery("select * from employee_master");
+
+
+            //Process the result
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                double sal = rs.getDouble(3);
+                System.out.printf("ID : %s , Name : %s, Salary : %s%n", id, name, sal);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                    rs = null;
+                }
+                if (stm != null) {
+                    stm.close();
+                    stm = null;
+                }
+                if (con != null) {
+                    con.close();
+                    con = null;
+                }
+                System.out.println("Disconnected from DB");
+            } catch (SQLException e) {
+            }
+        }
+    }
+
+}
