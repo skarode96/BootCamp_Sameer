2f532323797b11b26bcf5ae1fa3aee03648e676b
DS\karodes <skarode96@gmail.com>
2017-08-09 22:27:24 +0530

    DB using hibernate done

---
+++ Day19/HibernateDemo/src/com/example/client/Main.java
@@ -0,0 +1,21 @@
+package com.example.client;
+
+import com.cdk.dao.CourseDAO;
+import com.cdk.entity.Course;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("mysql");
+        CourseDAO courseDAO = new CourseDAO();
+//        courseDAO.save(managerFactory);
+//        courseDAO.update(managerFactory);
+//        courseDAO.read(managerFactory,2);
+        courseDAO.delete(managerFactory,3);
+        managerFactory.close();
+    }
+}
